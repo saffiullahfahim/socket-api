4342ea01e26d60984f710b1650619b9c10c791f6
saffiullahfahim <fahim14@student.sust.edu>
2022-12-27 23:46:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const https = require("https");
+
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("./ssl/api_saffiullahfahim_me.key"),
+  cert: fs.readFileSync("./ssl/api_saffiullahfahim_me.crt"),
+  ca: fs.readFileSync("./ssl/api_saffiullahfahim_me.ca-bundle"),
+};
+
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end("success");
+});
+
+// socket
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", async (socket) => {
+  socket.on("wow", async (data) => {
+    io.emit("wow", {
+      type: socket.handshake.auth.name,
+      data,
+    });
+  });
+});
+
+server.listen(6969, () => {
+  console.log(`app listening to port ${6969}`);
+});
+
